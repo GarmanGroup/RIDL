d76e110e9be84685b6b8772dbcb5079e0bcba5e1
charliebury <csbury@me.com>
2015-07-27 19:18:14 +0100

    new functionality added to ranking function - can now rank by Dmean and Dgain metrics as well as Dloss, and also choose how many output lines to print

--- specificDamageRanking.py
+++ specificDamageRanking.py
@@ -6,18 +6,21 @@ import numpy as np
 from scipy import stats
 
 class specificDamageRank(object):
-	def __init__(self, residueName = "", atomType = "", damageRank = 0 ,
-				 maxDensLossSlope = 0, slopeError = 0):
+	def __init__(self, residueName = "", atomType = "",densMetric = "",
+				 damageRank = 0 ,densSlope = 0, slopeError = 0):
 
 		self.residueName 		= residueName
 		self.atomType			= atomType
 		self.damageRank 		= damageRank
-		self.maxDensLossSlope 	= maxDensLossSlope
+		self.densSlope 			= densSlope
 		self.slopeError			= slopeError
+		self.densMetric			= densMetric
 
 class specificDamageRanking(object):
-	def __init__(self,atomList = []):
-		self.atomList = atomList
+	def __init__(self,atomList = [],densMetric = "",numLines = 0):
+		self.atomList 	= atomList
+		self.densMetric = densMetric # the density metric for which the ranking will be based
+		self.numLines 	= numLines # specify the number of lines of output to display to command line
 
 	def calculateRanks(self):
 		# for each atom within the atomList list of atom objects, group into 
@@ -25,6 +28,11 @@ class specificDamageRanking(object):
 		# lines of best fit for each residue/nucleotide type (for plots of 
 		# dataset number vs max density loss)
 
+		# don't run if density metric not specified
+		if self.densMetric == "":
+			print 'Need to specify density metric before this function can be run'
+			return
+	
 		residueDict = {}
 		for atom in self.atomList:
 			identifier = "{} {}".format(atom.basetype,atom.atomtype)
@@ -33,19 +41,26 @@ class specificDamageRanking(object):
 				residueDict[identifier]['slope'] = []
 				residueDict[identifier]['std_err'] = []
 
-			residueDict[identifier]['slope'].append(atom.maxDensLoss['lin reg']['slope'])
-			residueDict[identifier]['std_err'].append(atom.maxDensLoss['lin reg']['std_err'])
+			if str(self.densMetric).lower() == 'loss':
+				residueDict[identifier]['slope'].append(atom.maxDensLoss['lin reg']['slope'])
+				residueDict[identifier]['std_err'].append(atom.maxDensLoss['lin reg']['std_err'])
+			elif str(self.densMetric).lower() == 'mean':
+				residueDict[identifier]['slope'].append(atom.meanDensChange['lin reg']['slope'])
+				residueDict[identifier]['std_err'].append(atom.meanDensChange['lin reg']['std_err'])
+			elif str(self.densMetric).lower() == 'gain':
+				residueDict[identifier]['slope'].append(atom.maxDensGain['lin reg']['slope'])
+				residueDict[identifier]['std_err'].append(atom.maxDensGain['lin reg']['std_err'])
 
 		# calculate the average straight line slope for each residue/nucleotide type
 		specificDamageRanks = []
 		for key in residueDict.keys():
-			damageStats = specificDamageRank(key.split()[0],key.split()[1])
-			damageStats.maxDensLossSlope = np.mean(residueDict[key]['slope'])
+			damageStats = specificDamageRank(key.split()[0],key.split()[1],self.densMetric)
+			damageStats.densSlope = np.mean(residueDict[key]['slope'])
 			damageStats.slopeError = np.std(residueDict[key]['slope'])
 			specificDamageRanks.append(damageStats)
 
 		# rank residues/nucleotides by slope value
-		specificDamageRanks.sort(key = lambda x: x.maxDensLossSlope)
+		specificDamageRanks.sort(key = lambda x: x.densSlope)
 		counter = 0
 		for residue in specificDamageRanks:
 			residue.damageRank = counter
@@ -56,11 +71,25 @@ class specificDamageRanking(object):
 	def getDamageRanks(self):
 		# following on from the above function, output the calculated ordering of
 		# damage within the current structure
+
+		# don't run if damage ranks not yet determined above
+		try:
+			self.specificDamageRanks
+		except AttributeError:
+			print 'Need to calculate damage rankings before this function can be run'
+			return
+
 		print '--------------------------------------------------------------------'
-		print 'Ordering of damage as follows:'
-		for res in self.specificDamageRanks:
+		print 'Ordering of damage with D{} metric as follows:'.format(str(self.densMetric).lower())
+		
+		# if the number of output lines is specified, use this value, otherwise print all lines
+		if self.numLines != 0:
+			numLines = self.numLines
+		else:
+			numLines = len(self.specificDamageRanks)
+		for res in self.specificDamageRanks[0:numLines+1]:
 			print '{}\t{} {}\tSlope: {}\tStd Dev: {}'.format(str(res.damageRank),res.residueName, res.atomType,
-														  	 str(res.maxDensLossSlope)[:6], str(res.slopeError)[:6])
+															 str(res.densSlope)[:6], str(res.slopeError)[:6])
 
 
 
