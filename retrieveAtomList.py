c32434ae61599f08baa2525d6894d242373f965c
charliebury <csbury@me.com>
2015-08-20 05:47:53 +0100

    Now when processedAtom object list retrieved from pkl file, provides the doses recorded for the TRAP experiment

--- retrieveAtomList.py
+++ retrieveAtomList.py
@@ -7,6 +7,7 @@ from processedAtomList import processedAtomList
 def retrieveAtomList():
 	et = eTrack()
 	et.runPipeline('n','n','y','n','e_Track_inputfile_TRAP.txt')
-	atomList = processedAtomList(et.PDBmulti,10)
+	doses = [1.31,3.88,6.45,9.02,11.58,14.15,16.72,19.29,21.86,24.98,33.01,40.86,48.89,56.75,64.78,72.63]
+	atomList = processedAtomList(et.PDBmulti,10,doses[0:9])
 	atomList.processAtomList()
 	return atomList
