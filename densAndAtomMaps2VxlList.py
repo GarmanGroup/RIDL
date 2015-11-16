715bde469f9ff201db640b44149749bd2543bfcf
charliebury <csbury@me.com>
2015-11-16 10:15:36 +0000

    updated method for combining density and atom-tagged voxel lists into one entity (list of atom objects). Tested on 10**6 lists to be ~6 times quicker

--- densAndAtomMaps2VxlList.py
+++ densAndAtomMaps2VxlList.py
@@ -7,32 +7,19 @@ Created on Thu Jan 15 13:13:30 2015
 import sys 
 from progbar import progress
 
-###----------------###----------------###----------------###----------------###
-###############################################################################      
 # A class for .map file voxel
 class voxel_density:
     def __init__(self,atmnum=0,density=0): 
         self.atmnum = atmnum
         self.density = density
-###############################################################################
-###----------------###----------------###----------------###----------------###
-
+            
 def combinevxl_atmanddensvals(atmmap_list,densmap_list):
     # create list of voxel objects in class voxel_density 
+    # NEW VERSION OF FUNCTION - tested on 10**6 length lists (2s vs 12s)
     
-    vxl_list = []
-    appendvxl = vxl_list.append
+    vxl_list = [voxel_density(atm,dens) for atm,dens in zip(atmmap_list,densmap_list)]
     
-    atmmap_len = len(atmmap_list)
-    
-    for i in range(0,atmmap_len):
-        appendvxl(voxel_density(atmmap_list[i],densmap_list[i]))
-
-        # unessential progress bar here
-        progress(i+1, atmmap_len, suffix='')
-        
     return vxl_list
-            
                
 def combinevxl_atmanddensvals_gen(atmmap_list,densmap_list):
     # create list of voxel objects in class voxel_density 
