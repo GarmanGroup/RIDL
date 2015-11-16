238402c8096d797da42e89d2c980b17e58a66d68
charliebury <csbury@me.com>
2015-11-16 09:32:47 +0000

    minor change to the dose range included for a series of difference maps - want doses d2,..dn (n=10) here, not d1,..d(n-1) as was previously implemented - effectively x-axis (dose-axis) translated by the dose of 1 dataset

--- retrieveAtomList.py
+++ retrieveAtomList.py
@@ -8,6 +8,6 @@ def retrieveAtomList():
 	et = eTrack()
 	et.runPipeline('n','n','y','n','e_Track_inputfile_TRAP.txt')
 	doses = [1.31,3.88,6.45,9.02,11.58,14.15,16.72,19.29,21.86,24.98,33.01,40.86,48.89,56.75,64.78,72.63]
-	atomList = processedAtomList(et.PDBmulti,10,doses[0:9])
+	atomList = processedAtomList(et.PDBmulti,10,doses[1:10])
 	atomList.processAtomList()
 	return atomList
