10d570f737b571fa65a0622632e92e82bd1db0ff
charliebury <csbury@me.com>
2015-11-19 18:53:24 +0000

    additional care taken to test and flag at each point in job run if job fails to proceed

--- CADjob.py
+++ CADjob.py
@@ -1,55 +1,54 @@
 from ccp4Job import ccp4Job
+import os
 
 class CADjob():
 	# run CAD job to combine F and SIGF columns from two merged mtz files
-	def __init__(self,inputMtz1,inputMtz2,inputMtz3,Mtz1LabelName,Mtz2LabelName,Mtz3LabelName
-				 Mtz1LabelRename,Mtz2LabelRename,Mtz3LabelRename,outputMtzFile,outputDir,runLog):
+	def __init__(self,inputMtz1,inputMtz2,inputMtz3,Mtz1LabelName,Mtz2LabelName,Mtz3LabelName,
+				 Mtz1LabelRename,Mtz2LabelRename,Mtz3LabelRename,outputMtz,outputDir,runLog):
 
 		self.inputMtz1 			= inputMtz1 # mtz containing initial dataset Fs
 		self.inputMtz2 			= inputMtz2 # mtz containing later dataset Fs
 		self.inputMtz3 			= inputMtz3 # mtz containing phases
 		self.labels 			= [Mtz1LabelName,Mtz2LabelName,Mtz3LabelName]
 		self.renameLabels 		= [Mtz1LabelRename,Mtz2LabelRename,Mtz3LabelRename]
-
-		self.outputMtzFile		= outputMtzFile
+		self.outputMtz			= outputMtz
 		self.outputDir			= outputDir
 		self.runLog 			= runLog
-
-		runLog.writeToLog('Running CAD job')
-
-	def checkInputsExist(self):
-		# check if input mtz files exist
-		if (self.checkFileExists(self.CADinputMtz1) is False or
-		    self.checkFileExists(self.CADinputMtz2) is False):
-			string = 'Failed to find all required input files'
-			print string
-			runLog.writeToLog(string)
-			return False
-		else:
-			runLog.writeToLog('Input files:')
-			runLog.writeToLog('{}'.format(self.CADinputMtz1))	
-			runLog.writeToLog('{}'.format(self.CADinputMtz2))	
-			return True
+		self.runLog.writeToLog('Running CAD job')
 
 	def run(self):
 		if self.checkInputsExist() is False:
-			return
+			return False
 		self.runCAD()
 		if self.jobSuccess is True:
 			self.provideFeedback()
+			return True
 		else:
-			string = 'Job did not run successfully, see job log file "{}"',format(self.outputLogfile)
-			runLog.writeToLog(string)
-			return
+			self.runLog.writeToLog('Job did not run successfully, see job log file "{}"'.format(self.outputLogfile))
+			return False
+
+	def checkInputsExist(self):
+		# check if input mtz files exist
+		for fileName in (self.inputMtz1,self.inputMtz2,self.inputMtz3):
+			if os.path.isfile(fileName) is False:
+				self.runLog.writeToLog('Failed to find required input file "{}"'.format(fileName))
+				return False
+		else:
+			self.runLog.writeToLog('Input files:')
+			self.runLog.writeToLog('{}'.format(self.inputMtz1))	
+			self.runLog.writeToLog('{}'.format(self.inputMtz2))
+			self.runLog.writeToLog('Output files:')	
+			self.runLog.writeToLog('{}'.format(self.outputMtz))
+			return True
 
 	def runCAD(self):
 		title = 'run of cad'
 										 
 		self.commandInput1 	= 	'cad '+\
-								'HKLIN1 {} '.format(self.CADinputMtz1)+\
-								'HKLIN2 {} '.format(self.CADinputMtz2)+\
-								'HKLIN3 {} '.format(self.CADinputMtz3)+\
-								'HKLOUT {}'.format(self.CADoutputMtz)
+								'HKLIN1 {} '.format(self.inputMtz1)+\
+								'HKLIN2 {} '.format(self.inputMtz2)+\
+								'HKLIN3 {} '.format(self.inputMtz3)+\
+								'HKLOUT {}'.format(self.outputMtz)
 
 		self.commandInput2 	=	'title CAD JOB\n'+\
 								'monitor BRIEF\n'+\
@@ -81,7 +80,7 @@ class CADjob():
 		self.outputLogfile = 'CADlogfile.txt'
 
 		# run CAD job
-		job = ccp4Job('CAD',self.commandInput1,self.commandInput2,self.outputDir,self.outputLogfile,self.outputMtzFile)
+		job = ccp4Job('CAD',self.commandInput1,self.commandInput2,self.outputDir,self.outputLogfile,self.outputMtz)
 		self.jobSuccess = job.checkJobSuccess()
 
 	def provideFeedback(self):
@@ -89,5 +88,5 @@ class CADjob():
 		print '--------------------------'
 		print 'CAD Summary:'
 		print 'Input mtz files:\n{}\n{}\n{}'.format(self.inputMtz1,self.inputMtz2,self.inputMtz3)
-		print 'Output mtz file:\n {}'.format(self.outputMtzFile)
+		print 'Output mtz file:\n {}'.format(self.outputMtz)
 		print '--------------------------'
