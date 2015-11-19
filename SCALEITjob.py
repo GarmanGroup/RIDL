03f10c734c4ce477ec512481ae39e67c03731cf8
charliebury <csbury@me.com>
2015-11-19 18:54:23 +0000

    additional checks to ensure job runs successfully and flag at each stage if not

--- SCALEITjob.py
+++ SCALEITjob.py
@@ -1,39 +1,39 @@
 from ccp4Job import ccp4Job
+import os
 
 class SCALEITjob():
 	# run SCALEIT job to scale 2nd dataset Fs against 1st datasets	
-	def __init__(self,SCALEITinputMtz,SCALEIToutputMtz,Mtz1Label,Mtz2Label,outputDir,runLog):
-		self.SCALEITinputMtz 	= SCALEITinputMtz
-		self.SCALEIToutputMtz 	= SCALEIToutputMtz
-		self.Mtz1Label 			= Mtz1Label
-		self.Mtz2Label 			= Mtz2Label
-		self.outputDir 			= outputDir
-		self.runLog				= runLog
-
-		runLog.writeToLog('Running SCALEIT job')
-
-	def checkInputsExist(self):
-		# check if input mtz files exist
-		if self.checkFileExists(self.SCALEITinputMtz) is False:
-			string = 'Failed to find required input file'
-			print string
-			runLog.writeToLog(string)
-			return False
-		else:
-			runLog.writeToLog('Input files:')
-			runLog.writeToLog('{}'.format(self.SCALEITinputMtz))	
-			return True
+	def __init__(self,inputMtz,outputMtz,Mtz1Label,Mtz2Label,outputDir,runLog):
+		self.inputMtz 	= inputMtz
+		self.outputMtz 	= outputMtz
+		self.Mtz1Label 	= Mtz1Label
+		self.Mtz2Label 	= Mtz2Label
+		self.outputDir 	= outputDir
+		self.runLog		= runLog
+		self.runLog.writeToLog('Running SCALEIT job')
 
 	def run(self):
 		if self.checkInputsExist() is False:
-			return
+			return False
 		self.runSCALEIT()
 		if self.jobSuccess is True:
 			self.provideFeedback()
+			return True
 		else:
-			string = 'Job did not run successfully, see job log file "{}"',format(self.outputLogfile)
-			runLog.writeToLog(string)
-			return
+			self.runLog.writeToLog('Job did not run successfully, see job log file "{}"'.format(self.outputLogfile))
+			return False
+
+	def checkInputsExist(self):
+		# check if input mtz files exist
+		if os.path.isfile(self.inputMtz) is False:
+			self.runLog.writeToLog('Failed to find required input file')
+			return False
+		else:
+			self.runLog.writeToLog('Input files:')
+			self.runLog.writeToLog('{}'.format(self.inputMtz))	
+			self.runLog.writeToLog('Output files:')
+			self.runLog.writeToLog('{}'.format(self.outputMtz))	
+			return True
 
 	def runSCALEIT(self):
 		# run SCALEIT job to scale 2nd dataset Fs against 1st datasets
@@ -41,8 +41,8 @@ class SCALEITjob():
 
 		# run SCALEIT from command line
 		self.commandInput1 = 'scaleit '+\
-							 'HKLIN {} '.format(self.SCALEITinputMtz)+\
-							 'HKLOUT {}'.format(self.SCALEIToutputMtz)
+							 'HKLIN {} '.format(self.inputMtz)+\
+							 'HKLOUT {}'.format(self.outputMtz)
 		self.commandInput2 = 'title SCALEIT JOB\n'+\
 							 'NOWT\n'+\
 							 'converge NCYC 4\n'+\
@@ -59,13 +59,13 @@ class SCALEITjob():
 		self.outputLogfile = 'SCALEITlogfile.txt'
 
 		# run SCALEIT job
-		job = ccp4Job('SCALEIT',self.commandInput1,self.commandInput2,self.outputDir,self.outputLogfile,self.SCALEIToutputMtz)
+		job = ccp4Job('SCALEIT',self.commandInput1,self.commandInput2,self.outputDir,self.outputLogfile,self.outputMtz)
 		self.jobSuccess = job.checkJobSuccess()
 
 	def provideFeedback(self):
 		# provide some feedback
 		print '--------------------------'
-		print 'CAD Summary:'
-		print 'Input mtz files: {}'.format(self.SCALEITinputMtz)
-		print 'Output mtz file: {}'.format(self.SCALEIToutputMtz)
+		print 'SCALEIT Summary:'
+		print 'Input mtz files: {}'.format(self.inputMtz)
+		print 'Output mtz file: {}'.format(self.outputMtz)
 		print '--------------------------'
