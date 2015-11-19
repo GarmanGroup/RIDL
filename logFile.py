266af2f02d29511995fcde08f7c527d38faba15c
charliebury <csbury@me.com>
2015-11-19 18:56:04 +0000

    updated method to write log file initially when class is run

--- logFile.py
+++ logFile.py
@@ -3,15 +3,19 @@ from time import gmtime, strftime
 class logFile():
 	def __init__(self,fileName):
 		self.logFile = fileName
+		self.createLogFile()
 
-		fileName = open(self.logFile,'w')
-		fileName.write('Created at: {}'.format(self.getTime()))
+	def createLogFile(self):
+		log = open(self.logFile,'w')
+		log.write('Created at: {}\n'.format(self.getTime()))
+		log.close()
 
 	def writeToLog(self,logstring):
 		# write string to current log file
 		currentTime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 		with open(self.logFile, "a") as logfile:
 			logfile.write('{}\t{}\n'.format(self.getTime(),logstring))
+			print logstring
 
 	def getTime(self):
 		return strftime("%Y-%m-%d %H:%M:%S", gmtime())
