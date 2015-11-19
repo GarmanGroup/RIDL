7886c4f11041b9b016d870e9841547c97b5713ad
charliebury <csbury@me.com>
2015-11-19 17:39:40 +0000

    new class created for SCALEIT ccp4 program run

---
+++ SCALEITjob.py
@@ -0,0 +1,68 @@
+from ccp4Job import ccp4Job
+
+class SCALEITjob():
+	# run SCALEIT job to scale 2nd dataset Fs against 1st datasets	
+	def __init__(self,SCALEITinputMtz,SCALEIToutputMtz,Mtz1Label,Mtz2Label,outputDir,runLog):
+		self.SCALEITinputMtz 	= SCALEITinputMtz
+		self.SCALEIToutputMtz 	= SCALEIToutputMtz
+		self.Mtz1Label 			= Mtz1Label
+		self.Mtz2Label 			= Mtz2Label
+		self.outputDir 			= outputDir
+		self.runLog				= runLog
+
+		self.jobName 			= 'SCALEIT'
+		runLog.writeToLog('Running SCALEIT job')
+
+
+	def checkInputsExist(self):
+		# check if input mtz file exist
+		if self.checkFileExists(self.SCALEITinputMtz) is False:
+			return False
+		else:
+			runLog.writeToLog('Input files:')
+			runLog.writeToLog('{}'.format(self.SCALEITinputMtz))
+			return True
+
+	def run(self):
+		if self.checkInputsExist() is False:
+			return
+		self.runSCALEIT()
+		if self.jobSuccess is True:
+			self.provideFeedback()
+		else:
+			return
+
+	def runSCALEIT(self):
+		# run SCALEIT job to scale 2nd dataset Fs against 1st datasets
+		title = 'run of scaleit'
+
+		# run SCALEIT from command line
+		self.commandInput1 = 'scaleit '+\
+							 'HKLIN {} '.format(self.SCALEITinputMtz)+\
+							 'HKLOUT {}'.format(self.SCALEIToutputMtz)
+		self.commandInput2 = 'title SCALEIT JOB\n'+\
+							 'NOWT\n'+\
+							 'converge NCYC 4\n'+\
+							 'converge ABS 0.001\n'+\
+							 'converge TOLR -7\n'+\
+							 'REFINE ANISOTROPIC\n'+\
+							 'auto\n'+\
+							 'LABIN  FP=FP_{} SIGFP=SIGFP_{} -\n'.format(self.Mtz1Label,
+							 										 	 self.Mtz1Label)+\
+							 ' FPH1=FP_{} SIGFPH1=SIGFP_{}\n'.format(self.Mtz2Label,
+							  									 	 self.Mtz2Label)+\
+							 'END'
+
+		self.outputLogfile = 'SCALEITlogfile.txt'
+
+		# run SCALEIT job
+		job = ccp4Job('SCALEIT',self.commandInput1,self.commandInput2,self.outputDir,self.outputLogfile,self.SCALEIToutputMtz)
+		self.jobSuccess = job.checkJobSuccess()
+
+	def provideFeedback(self):
+		# provide some feedback
+		print '--------------------------'
+		print 'CAD Summary:'
+		print 'Input mtz files: {}'.format(self.SCALEITinputMtz)
+		print 'Output mtz file: {}'.format(self.SCALEIToutputMtz)
+		print '--------------------------'
