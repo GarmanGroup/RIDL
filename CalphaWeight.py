df91489fd97080f1c2e18eb1479aa98d5fd10d06
charliebury <csbury@me.com>
2015-11-19 12:20:19 +0000

    Calpha weight calculation now conducted on an indvidual metric basis as specified by input parameter to method

--- CalphaWeight.py
+++ CalphaWeight.py
@@ -6,9 +6,10 @@ import numpy as np
 class CalphaWeight(object):
 	def __init__(self, atomList = []):
 
-		self.atomList = atomList
+		self.atomList 	= atomList  # list of atoms over multiple doses
+		self.weight 	= {}  		# dictionary of weightings per metric type (mean, gain etc.)
 
-	def calculateWeights(self):
+	def calculateWeights(self,metric):
 		# collect the Calpha atoms and for each dataset number determine
 		# the Calpha weight 
 
@@ -25,28 +26,23 @@ class CalphaWeight(object):
 
 		# calculate the weighting for each dataset and for each density
 		# metric individually here
-		self.weight = {}
-		self.weight['loss'] = np.mean([atom.mindensity for atom in CalphaAtoms],0)
-		self.weight['gain'] = np.mean([atom.maxdensity for atom in CalphaAtoms],0)
-		self.weight['mean'] = np.mean([atom.meandensity for atom in CalphaAtoms],0)
+		self.weight[metric] = np.mean([atom.densMetric[metric]['Standard']['values'] for atom in CalphaAtoms],0)
 
-	def printWeights(self):
+	def printWeights(self,metric):
 		# print the Calpha weights per dataset to command line
 
 		# don't run function if weights have not been calculated yet
 		try:
-			self.weight['loss']
+			self.weight[metric]
 		except AttributeError:
-			print 'Need to calculate the weights first (use calculateWeights method)'
+			print 'Need to calculate the weights for metric "{}" first (use calculateWeights method)'.format(metric)
 			return 
 
 		print '-------------------------------------------------------'
 		print 'Calpha weights as follows:'
-		print '\tDataset\tDloss\tDmean\tDgain'
-		for counter in range(0,len(self.atomList[0].mindensity)):
-			print '\t{}\t{}\t{}\t{}'.format(counter,str(self.weight['loss'][counter])[:5],
-													str(self.weight['mean'][counter])[:5],
-													str(self.weight['gain'][counter])[:5])
+		print '\tDataset\tD{}'.format(metric)
+		for i in range(0,len(self.atomList[0].densMetric[metric]['Standard']['values'])):
+			print '\t{}:\t{}'.format(i+1,str(self.weight[metric][i])[:5])
 
 
 
