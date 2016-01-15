2a421655334ab1cddc72fe643bd2829ce5ea0553
charliebury <csbury@me.com>
2016-01-15 15:19:58 +0000

    new functions to save generic objects (not lists of objects) as .pkl files, and to retrieve them

--- savevariables.py
+++ savevariables.py
@@ -12,28 +12,28 @@ from progbar import progress
 # each dose level to save running time consuming scripts calculating 
 # electron density values to each atom in structure
 
-def save_objectlist(PDBlist,pdbname):
+def save_objectlist(PDBlist,pdbName):
     # to save current PDB list in a file:
-
-    filename = str(len(PDBlist))+'_'+str(pdbname)+'_data.pkl'
+    filename = str(len(PDBlist))+'_'+str(pdbName)+'_data.pkl'
     with open(filename, 'wb') as output:
         for atom in PDBlist:
             pickle.dump(atom, output, pickle.HIGHEST_PROTOCOL)
             
     return filename
 
-def retrieve_objectlist(filename):
+def retrieve_objectlist(fileName):
     # this function retrieves a list of objects from a file, given name
-    # of form filename = str(len(PDBlist))+'_'+str(pdbname)+'_data.pkl'
+    # of form filename = str(len(PDBlist))+'_'+str(pdbName)+'_data.pkl'
     print 'Retrieving dataset from .pkl file...'
+    checkFileFormat(fileName)
     
     #to determine number of atoms saved to file from file name:
-    num_atoms = (filename.split('/')[-1]).split('_')[0]
+    num_atoms = (fileName.split('/')[-1]).split('_')[0]
     print '\nNumber of atoms in file: ' + str(num_atoms)
         
     #to retrieve list from file to new list:
     PDBretrieved = []    
-    with open(str(filename), 'rb') as input:
+    with open(str(fileName), 'rb') as input:
         for i in range(0,int(num_atoms)):
             atom = None
             atom = pickle.load(input)
@@ -41,10 +41,29 @@ def retrieve_objectlist(filename):
 
             # unessential loading bar add-in
             progress(i+1, num_atoms, suffix='')
-
-                            
+       
     # return the list of atom objects  
     PDBretrieved.sort(key=lambda x: x.atomnum)
     print '\n---> success!'
     
     return PDBretrieved
+
+def saveGenericObject(obj,fileName):
+    # save a generic object to file name 'fileName'_data.pkl
+    filename = str(fileName)+'_data.pkl'
+    with open(filename, 'wb') as output:
+        pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
+    return filename
+
+def checkFileFormat(fileName):
+    if fileName.split('_')[-1] != 'data.pkl':
+        sys.exit('.pkl file of wrong format to retrieve. File {} supplied'.format(fileName))
+
+def retrieveGenericObject(fileName):
+    # retrieve a generic object from file name '{fileName}_data.pkl'
+    checkFileFormat(fileName)
+    with open(str(fileName), 'rb') as input:
+        obj = pickle.load(input)
+    print '\n---> success!'
+    return obj
+
