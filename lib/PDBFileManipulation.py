e6b0c2bcae16f14b8e74862415b74be9e4804de2
charliebury <csbury@me.com>
2016-03-20 19:05:24 +0000

    methods to write PDB file line for damage site with Bfactor column specified as Damage metric value now implemented

--- lib/PDBFileManipulation.py
+++ lib/PDBFileManipulation.py
@@ -35,6 +35,84 @@ def PDBtoList(pdbfilename,PDBarray):
     pdbin.close()
     return PDBarray
 
+def writePDBline_DamSite(atom,damValue,index):
+    # script to convert atom information (in class format) to 'ATOM' line format for
+    # pdb output files. Here the line is written in pdb format as a 'DAM' atom, 
+    # with same xyz coordinates as the original atom input into the function.
+    # The Bfactor column is written to be the damage metric value for that atom
+
+    #for 'atom' of PDBarray (ie a specific atom of the structure):
+    FIELD1 = "HETATM" #has length 6
+    FIELD2 = " "*(5-len(str(index))) + str(index) #has length 5
+    BREAK1 = " "*1 #has length 1
+    FIELD3 = str('O') + " "*(4-len('O')) #has length 4
+    FIELD4 = " " #has length 1
+    FIELD5 = str('DAM').rjust(3) #has length 3
+    BREAK2 = " " # has length 1
+    FIELD6 = str('A')
+    FIELD7 = " "*(4-len(str(index))) + str(index) #has length 4
+    FIELD8 = " " #has length 1
+    BREAK3 = " "*3 #has length 3
+    FIELD9 = "{0:8.3f}".format(atom.X_coord) #has length 8
+    FIELD10 = "{0:8.3f}".format(atom.Y_coord) #has length 8
+    FIELD11 = "{0:8.3f}".format(atom.Z_coord) #has length 8
+    FIELD12 = "{0:6.2f}".format(float(1)) #has length 6, fix occupancy here as 1
+    FIELD13 = "{0:6.3f}".format(float(damValue)) #has length 6
+    BREAK4 = " "*6 # has length 6
+    FIELD14 = " "*4 #has length 4
+    FIELD15 = str(atom.atomID).rjust(2) #has length 2
+    FIELD16 = " "*2 #has length 2
+    
+    line = FIELD1 + FIELD2 + BREAK1 + FIELD3 + FIELD4 + FIELD5 + BREAK2 + FIELD6 + FIELD7 + FIELD8 + BREAK3 + FIELD9 + FIELD10 + FIELD11 + FIELD12 + FIELD13 + BREAK4 + FIELD14 + FIELD15 + FIELD16
+
+    #as a check the length of the line should be 80 characters long:
+    if len(line) != 80:
+        sys.exit('Error: PDB ATOM line written with inconsistent length (should be 80 characters long)\n'+\
+                 '-> aborting line writing process, check atom object attributes for errors in formatting')        
+    return line
+
+
+def writePDBline_sudoWater(atom,Bfactor):
+    # script to convert atom information (in class format) to 'ATOM' line format for
+    # pdb output files. Here the line is written in pdb format as a sudo-water atom, 
+    # but with same xyz coordinates as the original atom input into the function.
+    # Here the Bfactor is written to be the value input by the user
+    
+    #for 'atom' of PDBarray (ie a specific atom of the structure):
+    FIELD1 = "HETATM" #has length 6
+    FIELD2 = " "*(5-len(str(atom.atomnum))) + str(atom.atomnum) #has length 5
+    BREAK1 = " "*1 #has length 1
+    FIELD3 = str('O') + " "*(4-len('O')) #has length 4
+    FIELD4 = " " #has length 1
+    FIELD5 = str('HOH').rjust(3) #has length 3
+    BREAK2 = " " # has length 1
+    FIELD6 = str('A')
+    FIELD7 = " "*(4-len(str(atom.residuenum))) + str(atom.residuenum) #has length 4
+    FIELD8 = " " #has length 1
+    BREAK3 = " "*3 #has length 3
+    FIELD9 = "{0:8.3f}".format(atom.X_coord) #has length 8
+    FIELD10 = "{0:8.3f}".format(atom.Y_coord) #has length 8
+    FIELD11 = "{0:8.3f}".format(atom.Z_coord) #has length 8
+    FIELD12 = "{0:6.2f}".format(float(1)) #has length 6, fix occupancy here as 1
+    FIELD13 = "{0:6.3f}".format(float(Bfactor)) #has length 6
+    BREAK4 = " "*6 # has length 6
+    FIELD14 = " "*4 #has length 4
+    FIELD15 = str(atom.atomID).rjust(2) #has length 2
+    FIELD16 = " "*2 #has length 2
+    
+    line = FIELD1 + FIELD2 + BREAK1 + FIELD3 + FIELD4 + FIELD5 + BREAK2 + FIELD6 + FIELD7 + FIELD8 + BREAK3 + FIELD9 + FIELD10 + FIELD11 + FIELD12 + FIELD13 + BREAK4 + FIELD14 + FIELD15 + FIELD16
+
+    #as a check the length of the line should be 80 characters long:
+    if len(line) != 80:
+        print 'Error: PDB ATOM line written with inconsistent length (should be 80 characters long)'
+        print '-> aborting line writing process, check atom object attributes for errors in formatting'
+        sys.exit()
+        
+    return line
+
+
+
+
 
 def convertPDBobject_toPDBline_fn(element,Occupancy):
     #script to convert atom information (in class format) to 'ATOM' line format for
@@ -81,7 +159,7 @@ def convertPDBobject_toPDBline_fn(element,Occupancy):
     FIELD13 = "{0:6.2f}".format(float(element.Bfactor)) #has length 6
     BREAK4 = " "*6 # has length 6
     FIELD14 = " "*4 #has length 4
-    FIELD15 = str(element.atomidentifier).rjust(2) #has length 2
+    FIELD15 = str(element.atomID).rjust(2) #has length 2
     FIELD16 = " "*2 #has length 2
     
     line = FIELD1 + FIELD2 + BREAK1 + FIELD3 + FIELD4 + FIELD5 + BREAK2 + FIELD6 + FIELD7 + FIELD8 + BREAK3 + FIELD9 + FIELD10 + FIELD11 + FIELD12 + FIELD13 + BREAK4 + FIELD14 + FIELD15 + FIELD16
@@ -95,7 +173,7 @@ def convertPDBobject_toPDBline_fn(element,Occupancy):
     return line
 
     
-def convertPDBobject_toPDBline_fn_Bfactorsetter(element,Bfactor):
+def writePDBline(element,Bfactor):
     #script to convert atom information (in class format) to 'ATOM' line format for
     #pdb output files. Note that the following PDB ATOM line convention is used:
         
@@ -117,54 +195,15 @@ def convertPDBobject_toPDBline_fn_Bfactorsetter(element,Bfactor):
     FIELD9 = "{0:8.3f}".format(element.X_coord) #has length 8
     FIELD10 = "{0:8.3f}".format(element.Y_coord) #has length 8
     FIELD11 = "{0:8.3f}".format(element.Z_coord) #has length 8
-    FIELD12 = "{0:6.2f}".format(float(element.Occupancy)) #has length 6
+    FIELD12 = "{0:6.2f}".format(float(1.00)) #has length 6 (occupancy set to 1.00 here by default)
     FIELD13 = "{0:6.2f}".format(float(Bfactor)) #has length 6
     BREAK4 = " "*6 # has length 6
     FIELD14 = " "*4 #has length 4
-    FIELD15 = str(element.atomidentifier).rjust(2) #has length 2
+    FIELD15 = str(element.atomID).rjust(2) #has length 2
     FIELD16 = " "*2 #has length 2
     
     line = FIELD1 + FIELD2 + BREAK1 + FIELD3 + FIELD4 + FIELD5 + BREAK2 + FIELD6 + FIELD7 + FIELD8 + BREAK3 + FIELD9 + FIELD10 + FIELD11 + FIELD12 + FIELD13 + BREAK4 + FIELD14 + FIELD15 + FIELD16
     
-    #as a check the length of the line should be 80 characters long:
-    if len(line) != 80:
-        print 'Error: PDB ATOM line written with inconsistent length (should be 80 characters long)'
-        print '-> aborting line writing process, check atom object attributes for errors in formatting'
-        sys.exit()
-        
-    return line
-
-
-def convertPDBobject_toPDBline_sudoWater(atom,Bfactor):
-    # script to convert atom information (in class format) to 'ATOM' line format for
-    # pdb output files. Here the line is written in pdb format as a sudo-water atom, 
-    # but with same xyz coordinates as the original atom input into the function.
-    # Here the Bfactor is written to be the value input by the user
-    
-    #for 'atom' of PDBarray (ie a specific atom of the structure):
-    FIELD1 = "HETATM" #has length 6
-    FIELD2 = " "*(5-len(str(atom.atomnum))) + str(atom.atomnum) #has length 5
-    BREAK1 = " "*1 #has length 1
-    FIELD3 = str('O') + " "*(4-len('O')) #has length 4
-    FIELD4 = " " #has length 1
-    FIELD5 = str('HOH').rjust(3) #has length 3
-    BREAK2 = " " # has length 1
-    FIELD6 = str('A')
-    FIELD7 = " "*(4-len(str(atom.residuenum))) + str(atom.residuenum) #has length 4
-    FIELD8 = " " #has length 1
-    BREAK3 = " "*3 #has length 3
-    FIELD9 = "{0:8.3f}".format(atom.X_coord) #has length 8
-    FIELD10 = "{0:8.3f}".format(atom.Y_coord) #has length 8
-    FIELD11 = "{0:8.3f}".format(atom.Z_coord) #has length 8
-    FIELD12 = "{0:6.2f}".format(float(1)) #has length 6, fix occupancy here as 1
-    FIELD13 = "{0:6.3f}".format(float(Bfactor)) #has length 6
-    BREAK4 = " "*6 # has length 6
-    FIELD14 = " "*4 #has length 4
-    FIELD15 = str(atom.atomidentifier).rjust(2) #has length 2
-    FIELD16 = " "*2 #has length 2
-    
-    line = FIELD1 + FIELD2 + BREAK1 + FIELD3 + FIELD4 + FIELD5 + BREAK2 + FIELD6 + FIELD7 + FIELD8 + BREAK3 + FIELD9 + FIELD10 + FIELD11 + FIELD12 + FIELD13 + BREAK4 + FIELD14 + FIELD15 + FIELD16
-
     #as a check the length of the line should be 80 characters long:
     if len(line) != 80:
         print 'Error: PDB ATOM line written with inconsistent length (should be 80 characters long)'
