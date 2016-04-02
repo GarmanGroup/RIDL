4ff655f8530f2e150c671ab41147d5043a815d03
charliebury <csbury@me.com>
2016-04-02 17:15:58 +0100

    new checks to determine whether user actually wants to use FOM columns in an input mtz file..the CAD input tags are adjusted accordingly to whether no column is used, a recalculated column is used, or a preset column from original input mtz file

--- lib/CADjob.py
+++ lib/CADjob.py
@@ -4,7 +4,7 @@ import os
 class CADjob():
 	# run CAD job to combine F and SIGF columns from two merged mtz files
 	def __init__(self,inputMtz1,inputMtz2,inputMtz3,Mtz1LabelName,Mtz2LabelName,Mtz3LabelName,
-				 Mtz1LabelRename,Mtz2LabelRename,Mtz3LabelRename,outputMtz,outputDir,runLog):
+				 Mtz1LabelRename,Mtz2LabelRename,Mtz3LabelRename,outputMtz,outputDir,runLog,FOMWeight):
 
 		self.inputMtz1 			= inputMtz1 # mtz containing initial dataset Fs
 		self.inputMtz2 			= inputMtz2 # mtz containing later dataset Fs
@@ -15,6 +15,17 @@ class CADjob():
 		self.outputDir			= outputDir
 		self.runLog 			= runLog
 		self.runLog.writeToLog('Running CAD job')
+
+		if FOMWeight == 'False':
+			self.FOMtagIn = ''
+			self.FOMtagOut = ''
+		elif FOMWeight == 'recalculate':
+			self.FOMtagIn = 'E3 = FOM{} \n'.format(self.labels[0])
+			self.FOMtagOut = 'E3 = FOM_{} \n'.format(self.renameLabels[0])
+		elif 'preset' in FOMWeight:
+			lbl = FOMWeight.split(',')[-1]
+			self.FOMtagIn = 'E3 = FOM{} \n'.format(lbl)
+			self.FOMtagOut = 'E3 = FOM_{} \n'.format(self.renameLabels[0])
 
 	def run(self):
 		inputFiles = [self.inputMtz1,self.inputMtz2,self.inputMtz3]
@@ -44,11 +55,11 @@ class CADjob():
 								'labin file 1 - \n'+\
 								'E1 = F{} - \n'.format(self.labels[0])+\
 								'E2 = SIGF{} - \n'.format(self.labels[0])+\
-								'E3 = FOM{} \n'.format(self.labels[0])+\
+								'{}'.format(self.FOMtagIn)+\
 								'labout file 1 - \n'+\
 								'E1 = FP_{} - \n'.format(self.renameLabels[0])+\
 								'E2 = SIGFP_{} - \n'.format(self.renameLabels[0])+\
-								'E3 = FOM_{} \n'.format(self.renameLabels[0])+\
+								'{}'.format(self.FOMtagOut)+\
 								'ctypin file 1 - \n'+\
 								'E1 = F - \n'+\
 								'E2 = Q - \n'+\
@@ -75,10 +86,20 @@ class CADjob():
 		job = ccp4Job('CAD',self.commandInput1,self.commandInput2,self.outputDir,self.outputLogfile,self.outputMtz)
 		self.jobSuccess = job.checkJobSuccess()
 
-	def provideFeedback(self):
+	def provideFeedback(self,includeDir=False):
 		# provide some feedback
+		if includeDir is False:
+			fileIn1  = self.inputMtz1.split('/')[-1]
+			fileIn2  = self.inputMtz2.split('/')[-1]
+			fileIn3  = self.inputMtz3.split('/')[-1]
+			fileOut = self.outputMtz.split('/')[-1]
+		else:
+			fileIn1  = self.inputMtz1
+			fileIn2  = self.inputMtz2
+			fileIn3  = self.inputMtz3
+			fileOut = self.outputMtz
 		print '--------------------------'
 		print 'CAD Summary:'
-		print 'Input mtz files:\n{}\n{}\n{}'.format(self.inputMtz1,self.inputMtz2,self.inputMtz3)
-		print 'Output mtz file:\n {}'.format(self.outputMtz)
+		print 'Input mtz files: {}\n{}{}\n{}{}'.format(fileIn1,' '*17,fileIn2,' '*17,fileIn3)
+		print 'Output mtz file: {}'.format(fileOut)
 		print '--------------------------'
