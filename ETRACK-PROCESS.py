ede30cc71e9b921f17b3a99976dfba6152075b92
charliebury <csbury@me.com>
2016-04-11 23:54:56 +0100

    updated command line parser to allow main ETRACK scripts to be run as well as processing scripts

--- ETRACK-PROCESS.py
+++ ETRACK-PROCESS.py
@@ -1,9 +1,18 @@
 import argparse
 from runProcessFiles import process
 
-parser = argparse.ArgumentParser(description='Process input files for ETRACK job')
-parser.add_argument('-i', type=str, dest='inputFile', action='store',
+parser = argparse.ArgumentParser(description='Run the ETRACK pipeline from the command line')
+
+parser.add_argument('-i',type=str,dest='inputFile',action='store',
                    help='An input file containing pdb and mtz file names, as well as relevant mtz column labels')
 
+parser.add_argument('-p',dest='process',action='store_const',default=False,
+                   help='Generate electron density difference maps',const=True)
+
+parser.add_argument('-c',dest='calculate',action='store_const',default=False,
+                   help='Calculate damage metrics per atom',const=True)
+
 args = parser.parse_args()
-p = process(inputFile=args.inputFile)
+
+if args.process is True:
+	p = process(inputFile=args.inputFile,proceedToETRACK=args.calculate)
