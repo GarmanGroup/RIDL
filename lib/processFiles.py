c7ddd05fdcabbf5f8bb5b9a13be37c1c61aaa82a
charliebury <csbury@me.com>
2016-04-18 23:19:33 +0100

    new code included to keep the generated FC map as one of the key maps to not delete at end of job run

--- lib/processFiles.py
+++ lib/processFiles.py
@@ -2,20 +2,24 @@ from CADSCALEITpiped import pipeline as pipe1
 from SFALLFFTpiped import pipeline as pipe2
 import os
 import shutil
-from runETRACK import run as runETRACK
 
 class processFiles():
-	def __init__(self,inputFile='',proceedToETRACK=False):
+
+	def __init__(self,
+				 inputFile       = '',
+				 proceedToETRACK = False):
+
 		self.inputFile 		 = inputFile
 		self.proceedToETRACK = proceedToETRACK
 
 	def runProcessing(self):
+
 		success = self.readMainInputFile()
 		if success == False:
 			return
+
 		self.checkOutputDirExists()
 		self.findFilesInDir()
-
 		self.checkForMultipleDatasets()
 
 		try:
@@ -34,6 +38,8 @@ class processFiles():
 			for i in range(self.numDsets):
 				self.getCurrentInputParams(jobNumber=i)
 				success = self.runPipelines()
+				if success is False:
+					return success
 
 		if success is True:
 			if self.proceedToETRACK is True:
@@ -52,9 +58,9 @@ class processFiles():
 				if self.deleteIntermediateFiles.lower() == 'true':
 					success = self.cleanUpDir()
 				else:
-					success = self.cleanUpDir(removeMtzs=False,
-											  removeMaps=False,
-											  removePdbs=False)
+					success = self.cleanUpDir(removeMtzs = False,
+											  removeMaps = False,
+											  removePdbs = False)
 		return success
 
 	def readMainInputFile(self):
@@ -83,11 +89,23 @@ class processFiles():
 
 	def checkAllRequiredInputsFound(self):
 		# check that all required properties have been found
-		requiredProps = ['name1','mtz1','mtzlabels1','pdb1','RfreeFlag1',
-						 'name2','mtz2','mtzlabels2','pdb2',
-						 'name3','mtz3','mtzlabels3',''
-						 'sfall_VDWR','densMapType','dir',
-						 'FFTmapWeight','deleteIntermediateFiles']
+		requiredProps = ['name1',
+						 'mtz1',
+						 'mtzlabels1',
+						 'pdb1',
+						 'RfreeFlag1',
+						 'name2',
+						 'mtz2',
+						 'mtzlabels2',
+						 'pdb2',
+						 'name3',
+						 'mtz3',
+						 'mtzlabels3',
+						 'sfall_VDWR',
+						 'densMapType',
+						 'dir',
+						 'FFTmapWeight',
+						 'deleteIntermediateFiles']
 
 		for prop in requiredProps:
 			try:
@@ -125,6 +143,7 @@ class processFiles():
 			else:
 				if self.name1 == self.name2:
 					error = True
+
 			if error is True:
 				print '"name1" and "name2" inputs must be different for each job in batch'
 				return False
@@ -132,22 +151,44 @@ class processFiles():
 		if self.densMapType not in ('DIFF','SIMPLE','2FOFC','END'):
 			print '"densMapType" input of incompatible format, default is DIFF'
 			return False
+
 		if self.deleteIntermediateFiles.lower() not in ('true','false'):
-			print '"deleteIntermediateFiles" input of incompatible format (true,false) - case insensitive'
+			str = '"deleteIntermediateFiles" input of incompatible '+\
+				  'format (true,false) - case insensitive'
+			print str
 			return False
 		try:
 			float(self.sfall_VDWR)
 		except ValueError:
 			print '"sfall_VDWR" input must be a float'
 			return False
+
 		if float(self.sfall_VDWR) <= 0:
 			print '"sfall_VDWR" input must be a positive float'
 			return False
 
+		if 'preset' not in self.FFTmapWeight and self.FFTmapWeight not in ('recalculate','False'):
+			str = '"FFTmapWeight" input must take value in ("recalculate", '+\
+				  '"False","preset,x") where "x" is the FOM weight label '+\
+				  '(of the form FOMx) of a preset FOM column within the '+\
+				  'input .mtz file'
+			print str
+			return False
+
+		if self.FFTmapWeight.startswith('preset') is True:
+			if self.FFTmapWeight.replace('preset','')[0] != ',':
+				str = 'If "FFTmapWeight" input is specified by "preset", '+\
+					  'then this must be followed a comma and then the FOM '+\
+					  'label name within the input .mtz file (i.e. "preset,x" '+\
+					  'for column label "FOMx"). If column label is simply '+\
+					  '"FOM" then use "preset,"'
+				print str
+				return False
+
 		print 'All input file parameters appear to be of suitable format'
 		return True
 
-	def checkFileExists(self,fileName=''):
+	def checkFileExists(self, fileName = ''):
 		if os.path.isfile(fileName) and os.access(fileName,os.R_OK):
 			return True
 		else:
@@ -202,7 +243,7 @@ class processFiles():
 			os.makedirs(self.dir)
 		print 'Working directory set to "{}"'.format(self.dir)
 
-	def getCurrentInputParams(self,jobNumber=0):
+	def getCurrentInputParams(self, jobNumber = 0):
 		# select correct input parameters if multiple jobs within main input file.
 		# used when writing separate input files for each part of pipeline.
 		props1 = ['name2','mtz2','mtzlabels2','pdb2']
@@ -231,20 +272,20 @@ class processFiles():
 
 		self.pipe1FileName 	= self.dir+self.inputFile.split('.')[0]+'_cadscaleit.txt'
 
-		props = {'mtzIn1':'mtz1',
-				 'Mtz1LabelName':'mtzlabels1_current',
-				 'Mtz1LabelRename':'name1_current',
-				 'RfreeFlag1':'RfreeFlag1_current',
-				 'mtzIn2':'mtz2_current',
-				 'Mtz2LabelName':'mtzlabels2_current',
-				 'Mtz2LabelRename':'name2_current',
-				 'mtzIn3':'mtz3',
-				 'Mtz3LabelName':'mtzlabels3',
-				 'Mtz3LabelRename':'name3',
-				 'inputPDBfile':'pdb2_current',
-				 'densMapType':'densMapType',
-				 'deleteMtzs':'deleteIntermediateFiles',
-				 'FFTmapWeight':'FFTmapWeight'}
+		props = {'mtzIn1'         : 'mtz1',
+				 'Mtz1LabelName'  : 'mtzlabels1_current',
+				 'Mtz1LabelRename': 'name1_current',
+				 'RfreeFlag1'     : 'RfreeFlag1_current',
+				 'mtzIn2'         : 'mtz2_current',
+				 'Mtz2LabelName'  : 'mtzlabels2_current',
+				 'Mtz2LabelRename': 'name2_current',
+				 'mtzIn3'         : 'mtz3',
+				 'Mtz3LabelName'  : 'mtzlabels3',
+				 'Mtz3LabelRename': 'name3',
+				 'inputPDBfile'   : 'pdb2_current',
+				 'densMapType'    : 'densMapType',
+				 'deleteMtzs'     : 'deleteIntermediateFiles',
+				 'FFTmapWeight'   : 'FFTmapWeight'}
 
 		fileOut1 = open(self.pipe1FileName,'w')
 		inputString = ''
@@ -265,13 +306,13 @@ class processFiles():
 		else:
 			self.mtzIn = '{}{}_sigmaa.mtz'.format(self.dir,self.name2_current)
 
-		props = {'pdbIN':'pdb2_current',
-				 'initialPDB':'name1_current',
-				 'laterPDB':'name2_current',
-				 'sfall_VDWR':'sfall_VDWR',
-				 'mtzIN':'mtzIn',
-				 'densMapType':'densMapType',
-				 'FFTmapWeight':'FFTmapWeight'}
+		props = {'pdbIN'       : 'pdb2_current',
+				 'initialPDB'  : 'name1_current',
+				 'laterPDB'    : 'name2_current',
+				 'sfall_VDWR'  : 'sfall_VDWR',
+				 'mtzIN'       : 'mtzIn',
+				 'densMapType' : 'densMapType',
+				 'FFTmapWeight': 'FFTmapWeight'}
 
 		fileOut2 = open(self.pipe2FileName,'w')
 		inputString = ''
@@ -282,9 +323,9 @@ class processFiles():
 		fileOut2.close()
 
 	def runPipeline1(self):
-		self.p1 = pipe1(outputDir=self.dir,
-						inputFile=self.pipe1FileName,
-						jobName=self.jobName)
+		self.p1 = pipe1(outputDir = self.dir,
+						inputFile = self.pipe1FileName,
+						jobName   = self.jobName)
 
 		outcome = self.p1.runPipeline()
 		if outcome == 0:
@@ -295,9 +336,9 @@ class processFiles():
 			return False
 
 	def runPipeline2(self):
-		self.p2 = pipe2(outputDir=self.dir,
-						inputFile=self.pipe2FileName,
-						jobName=self.jobName)
+		self.p2 = pipe2(outputDir = self.dir,
+						inputFile = self.pipe2FileName,
+						jobName   = self.jobName)
 
 
 		outcome = self.p2.runPipeline()
@@ -312,8 +353,13 @@ class processFiles():
 		# find files initially in working directory
 		self.filesInDir = os.listdir(self.dir)
 
-	def cleanUpDir(self,removeMtzs=True,removeMaps=True,removePdbs=True):
+	def cleanUpDir(self,
+				   removeMtzs   = True,
+				   removeMaps   = True,
+				   removePdbs   = True,
+				   includeFCmap = True):
 		# after successful completion of pipeline clean up working directory
+
 		print '\nCleaning up working directory'
 
 		# distinguish between FFT and END map output formats depending on program used (FFT/END shellscript)
@@ -322,44 +368,68 @@ class processFiles():
 		else:
 			densMapProg = 'fft'
 
-		params 			= [self.dir,self.dsetName]
-		renameParams	= [self.dir,self.name2_current]
-		keyLogFiles 	= [self.p1.runLog.logFile,self.p2.runLog.logFile]
-		mapFiles 		= ['{}{}_{}_cropped_cropped.map'.format(self.dir,self.dsetName,densMapProg),
-						   '{}{}_sfall_cropped.map'.format(*params)]
-		pdbFiles 		= ['{}{}_reordered.pdb'.format(*params)]
-		outputFiles 	= mapFiles + pdbFiles
-		renameFiles 	= ['{}{}_density.map'.format(*renameParams),
-					  	   '{}{}_atoms.map'.format(*renameParams),
-					   	   '{}{}.pdb'.format(*renameParams)]
+		params 		  = [self.dir,self.dsetName]
+		renameParams  = [self.dir,self.name2_current]
+		renameParams2 = [self.dir,self.name1_current]
 
-		subdir = '{}{}_additionalFiles/'.format(self.dir,self.jobName)
+		keyLogFiles  = [self.p1.runLog.logFile,self.p2.runLog.logFile]
+
+		if self.densMapType != 'END':
+			mapFiles = ['{}{}-{}-{}_cropped_cropped.map'.format(self.dir,
+																self.dsetName,
+																self.densMapType,
+																densMapProg)]
+		else:
+			mapFiles = ['{}{}-{}_cropped_cropped.map'.format(self.dir,
+															 self.dsetName,
+															 densMapProg)]
+		mapFiles += ['{}{}_sfall_cropped.map'.format(*params)]
+
+		renameMaps = ['{}{}_density.map'.format(*renameParams),
+					   '{}{}_atoms.map'.format(*renameParams)]
+
+		if includeFCmap is True:
+			mapFiles += ['{}{}-FC-{}_cropped_cropped.map'.format(self.dir,
+																 self.dsetName,
+																 densMapProg)]
+			renameMaps += ['{}{}_FC.map'.format(*renameParams2)]
+
+		pdbFiles  = ['{}{}_reordered.pdb'.format(*params)]
+		renamePDB = ['{}{}.pdb'.format(*renameParams)]
+
+		outputFiles = mapFiles + pdbFiles
+		renameFiles = renameMaps + renamePDB
+
+		subdir = '{}{}_additionalFiles/'.format(self.dir,
+												self.jobName)
 		os.system('mkdir {}'.format(subdir))
 
+		keyFiles = keyLogFiles + mapFiles + pdbFiles
 		for file in os.listdir(self.dir): 
 			if file.endswith('_additionalFiles'):
 				continue
 			if file not in self.filesInDir:
 				fileName = '{}{}'.format(self.dir,file)
-				if fileName not in keyLogFiles+mapFiles+pdbFiles:
+				if fileName not in keyFiles:
 					os.system('mv {} {}{}'.format(fileName,subdir,file))
 
 		for file in os.listdir(subdir):
 			remove = False
 			if removeMtzs is True and file.endswith('.mtz'):
-					remove = True
+				remove = True
 			if removeMaps is True and file.endswith('.map'):
-					remove = True
+				remove = True
 			if removePdbs is True and file.endswith('.pdb'):
-					remove = True
+				remove = True
 			if remove is True:
 				os.system('rm {}{}'.format(subdir,file))
 
 		shutil.make_archive(subdir, 'zip', subdir)
 		os.system('rm -rf {}'.format(subdir))
 
-		# rename final map files
-		for i in range(3): os.system('mv {} {}'.format(outputFiles[i],renameFiles[i]))
+		# rename final map & pdb files
+		for i in range(len(outputFiles)): 
+			os.system('mv {} {}'.format(outputFiles[i],renameFiles[i]))
 
 		# move initial dataset pdb files to working directory
 		self.moveInitialPDBfile()
@@ -373,35 +443,65 @@ class processFiles():
 		return True
 
 	def moveInitialPDBfile(self):
-		# copy the initial dataset pdb file to the working directory (useful if further ETRACK jobs to be run)
+		# copy the initial dataset pdb file to the working directory
+		# (useful if further ETRACK jobs to be run)
+
 		if self.multiDatasets == False or self.repeatedFile1InputsUsed == True:
-			os.system('cp {} {}{}'.format(self.pdb1,self.dir,self.pdb1.split('/')[-1]))
+
+			os.system('cp {} {}{}'.format(self.pdb1,
+										  self.dir,
+										  self.pdb1.split('/')[-1]))
 		else:
 			for pdb in self.pdb1.split(','):
-				os.system('cp {} {}{}'.format(pdb,self.dir,pdb.split('/')[-1]))
 
-	def writeETRACKInputFile(self,write=True,run=True):
-		# writes an input file for the run of ETRACK after this processing pipeline has completed.
-		# allows the user to immediately run the rest of the program, IF the user did specify 
-		# all required datasets within a damage series. If the user only processed a subset of 
-		# required datasets within a damage series, another ETRACK input file will need to be manually 
-		# created to run the rest of the program with ALL datasets within the damage series included.
+				os.system('cp {} {}{}'.format(pdb,
+											  self.dir,
+											  pdb.split('/')[-1]))
+
+	def writeETRACKInputFile(self,
+							 write    = True,
+							 run      = True,
+							 cleanUp  = True,
+							 imports  = True):
+		# writes an input file for the run of ETRACK after this processing 
+		# pipeline has completed. Allows the user to immediately run the 
+		# rest of the program, IF the user did specify all required datasets 
+		# within a damage series. If the user only processed a subset of 
+		# required datasets within a damage series, another ETRACK input 
+		# file will need to be manually created to run the rest of the 
+		# program with ALL datasets within the damage series included.
+
 		if write is False:
 			return
 
-		r = runETRACK(runAll=False)
+		if imports is True:
+			from runETRACK import run as runETRACK
+
+		r = runETRACK(runAll = False)
 		seriesName = self.dir.split('/')[-2]
 		doses = ','.join(map(str,range(len(self.name2.split(','))))) # TODO: implement proper doses here
 
-		r.writeInputFile(inDir=self.dir,
-						 outDir=self.dir,
-						 damageset_name=seriesName,
-						 laterDatasets=self.name2,
-						 initialPDB=self.pdb1.split('/')[-1],
-						 doses=doses)
-		os.system('mv {} {}{}'.format(r.inputFileName,self.dir,r.inputFileName))
+		r.writeInputFile(inDir         = self.dir,
+						 outDir        = self.dir,
+						 damSetName    = seriesName,
+						 laterDatasets = self.name2,
+						 initialPDB    = self.pdb1.split('/')[-1],
+						 doses         = doses)
+
+		os.system('mv {} {}/{}'.format(r.inputFileName,self.dir,r.inputFileName))
 
 		if run is True:
-			r = runETRACK(inputFileLoc=self.dir)
+			r = runETRACK(inputFileLoc = self.dir)
+
+		if cleanUp is True:
+			newDir = self.dir+'perDatasetMapFiles/'
+			os.system('mkdir {}'.format(newDir))
+			for f in os.listdir(self.dir):
+				for fType in ['.pdb','.zip','.log','.map']:
+					if f.endswith(fType):
+						os.system('mv {}{} {}{}'.format(self.dir,f,newDir,f))
+			os.system('mv {} {}ETRACK_output/'.format(newDir,self.dir))
+
+
 
 
