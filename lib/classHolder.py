dede045f3268035a2326854e183f9d21941acf51
charliebury <csbury@me.com>
2016-10-05 11:35:51 +0100

    new methods added to class to convert 1d list of voxels into a 3d array of voxels with xyz cartesian positions calculated. Symmetry operations now also parsed from the input map file and used to assign all suitable voxels to an atom (perhaps using symmetry operations to do so)

--- lib/classHolder.py
+++ lib/classHolder.py
@@ -207,6 +207,13 @@ class StructurePDB(object):
                                   self.atomtype)
         return ID
 
+    def getXYZ(self):
+
+        # get a vector of x x z for atomic position
+
+        xyz = [self.X_coord,self.Y_coord,self.Z_coord]
+        return xyz
+
     def getProtonNumber(self,
                         printText = True):
 
@@ -394,46 +401,46 @@ class MapInfo:
     # A class for .map file header info
     
     def __init__(self, 
-                 nx            = 0, 
-                 ny            = 0, 
-                 nz            = 0, 
-                 mapType       = 0, 
-                 start1        = 0, 
-                 start2        = 0, 
-                 start3        = 0,
-                 gridsamp1     = 0, 
-                 gridsamp2     = 0, 
-                 gridsamp3     = 0, 
-                 celldim_a     = 0, 
-                 celldim_b     = 0,
-                 celldim_c     = 0, 
-                 celldim_alpha = 0, 
-                 celldim_beta  = 0, 
-                 celldim_gamma = 0,
-                 fast_axis     = 0, 
-                 med_axis      = 0, 
-                 slow_axis     = 0, 
-                 mindensity    = 0, 
-                 maxdensity    = 0,
-                 meandensity   = 0,
-                 vxls_val      = []):
+                 nx             = 0, 
+                 ny             = 0, 
+                 nz             = 0, 
+                 mapType        = 0, 
+                 start1         = 0, 
+                 start2         = 0, 
+                 start3         = 0,
+                 gridsamp1      = 0, 
+                 gridsamp2      = 0, 
+                 gridsamp3      = 0, 
+                 celldim_a      = 0, 
+                 celldim_b      = 0,
+                 celldim_c      = 0, 
+                 celldim_alpha  = 0, 
+                 celldim_beta   = 0, 
+                 celldim_gamma  = 0,
+                 fast_axis      = 0, 
+                 med_axis       = 0, 
+                 slow_axis      = 0, 
+                 mindensity     = 0, 
+                 maxdensity     = 0,
+                 meandensity    = 0,
+                 vxls_val       = []):
             
         self.type       = mapType
         self.nxyz       = {'nx' : nx,
                            'ny' : ny,
                            'nz' : nz}
-        self.start      = {'1' : start1,
-                           '2' : start2,
-                           '3' : start3}
+        self.start      = {'fast' : start1,
+                           'med' : start2,
+                           'slow' : start3}
         self.gridsamp   = {'1' : gridsamp1,
                            '2' : gridsamp2,
                            '3' : gridsamp3}
         self.celldims   = {'a' : celldim_a,
                            'b' : celldim_b,
                            'c' : celldim_c,
-                          'alpha' : celldim_alpha,
-                          'beta'  : celldim_beta,
-                          'gamma' : celldim_gamma}
+                           'alpha' : celldim_alpha,
+                           'beta'  : celldim_beta,
+                           'gamma' : celldim_gamma}
         self.axis       = {'fast' : fast_axis,
                            'med'  : med_axis,
                            'slow' : slow_axis}
@@ -441,6 +448,149 @@ class MapInfo:
                            'max'  : maxdensity,
                            'mean' : meandensity}
         self.vxls_val   = vxls_val
+
+        # specify params for conversion from fractional
+        # unit cell indices to xyz coordinates here
+        # so not computed for each individual voxel
+
+    def getSymOps(self,
+                  Xvals = [],
+                  Yvals = [],
+                  Zvals = []):
+
+        # take unformatted sym operations from map file
+        # header and convert to a set of usable operations
+
+        try:
+            self.symOpsUnformatted
+        except AttributeError:
+            return
+
+        symOpsFormatted = []
+        for symOp in self.symOpsUnformatted:
+            symOpFormatted = [[],[],[]]
+            print symOp
+            l = symOp.replace(' ','').split(',')
+            for i in range(len(Xvals)):
+                X = Xvals[i]
+                Y = Yvals[i]
+                Z = Zvals[i]   
+                for k,li in enumerate(l):
+                    lnew = ''
+                    li2 = li.replace('/','./')
+                    for j,c in enumerate(li2):
+                        if c in ['X','Y','Z']:
+                            if j == 0:
+                                lnew += c
+                            else:
+                                try:
+                                    int(li2[j-1])
+                                    lnew += '*'+c
+                                except ValueError:
+                                    lnew += c
+                        else:
+                            lnew += c
+                    symOpFormatted[k].append(eval(lnew))
+            symOpsFormatted.append(symOpFormatted)
+        return symOpsFormatted
+
+    def reshape1dTo3d(self):
+
+        # reshape the 1d list of voxels to 3d
+
+        numVoxels = self.nxyz['nx']*self.nxyz['ny']*self.nxyz['nz']
+        nxyz_keys = ['nx','ny','nz']
+        m = np.arange(numVoxels).reshape((self.nxyz[nxyz_keys[int(self.axis['slow'])-1]],
+                                          self.nxyz[nxyz_keys[int(self.axis['med'])-1]],
+                                          self.nxyz[nxyz_keys[int(self.axis['fast'])-1]]))
+
+        self.voxels3d = m
+
+    def get3dVoxPosn(self,
+                     voxPos1d = 0):
+
+        # for a 1d voxel position within a list and unit 
+        # cell parameters determine the xyz position of
+        # the voxel in the asymmetric unit
+
+        axes = ['fast','med','slow']
+        q   = self.nxyz['ny']*self.nxyz['nx']
+        k   = voxPos1d/q + 1 
+        k1  = voxPos1d%q
+        i   = k1%self.nxyz['nx'] + 1
+        j   = k1/self.nxyz['nx'] + 1
+
+        ijk = np.array([i,j,k]) + np.array([self.start[ax] for ax in axes])
+
+        order = [self.axis[m] for m in axes]
+        ijk_ordered = [x for (y,x) in sorted(zip(order,list(ijk)))]
+
+        return ijk_ordered
+
+    def abs2xyz_params(self):
+
+        # parameters to convert from abc fractional 
+        # coordinates to xyz cartesian system
+
+        a   = self.celldims['a']
+        b   = self.celldims['b']
+        c   = self.celldims['c']
+        alp = np.pi*(float(self.celldims['alpha'])/180)
+        bet = np.pi*(float(self.celldims['beta'])/180)
+        gam = np.pi*(float(self.celldims['gamma'])/180)
+        v   = np.sqrt(1 - np.cos(alp)**2 - np.cos(bet)**2 - np.cos(gam)**2 + 2*np.cos(alp)*np.cos(bet)*np.cos(gam))
+        M   = np.array([[a,b*np.cos(gam),c*np.cos(bet)],
+                       [0,b*np.sin(gam),c*(np.cos(alp)-np.cos(bet)*np.cos(gam))/np.sin(gam)],
+                       [0,0,float(c*v)/np.sin(gam)]])
+
+        self.abc2xyzMatrix = M
+
+    def abc2xyz(self,
+                asymIndices = [],
+                fracInput   = False,
+                coordType   = 'cartesian'):
+
+        # convert from abc coordinates to xyz coordinates 
+        # coordType takes 'cartesian' or 'fractional'
+
+        if not fracInput:
+            xyzFrac = [np.array(map(float,asymIndices[0]))/self.gridsamp['1'],
+                       np.array(map(float,asymIndices[1]))/self.gridsamp['2'],
+                       np.array(map(float,asymIndices[2]))/self.gridsamp['3']]
+        else:
+            xyzFrac = asymIndices           
+        
+        if coordType == 'cartesian':
+            xyz     = np.dot(self.abc2xyzMatrix,xyzFrac)
+            return xyz
+        elif coordType == 'fractional':
+            return xyzFrac
+
+    def getVoxXYZ(self,
+                  vox1Dindices = 0,
+                  coordType    = 'fractional'):
+
+        # find the cartesian xyz location for a voxel in asym unit
+        # coordType takes 'cartesian' or 'fractional'
+
+        ijkFull = [[],[],[]]
+        for i in vox1Dindices:
+            ijk = self.get3dVoxPosn(i)
+            for a,b in enumerate(ijk):
+                ijkFull[a].append(b) 
+
+        xyz = self.abc2xyz(asymIndices = ijkFull,
+                           coordType   = coordType)
+
+        xyzNeat = []
+        if coordType == 'cartesian':
+            rnd = 2
+        else:
+            rnd = 3
+        for l in range(len(xyz[0])):
+            xyzNeat.append([round(xyz[m][l],rnd) for m in range(3)])
+
+        return xyzNeat
 
     def getnxyzString(self):
 
@@ -455,7 +605,7 @@ class MapInfo:
         # get string of grid sampling start positions
 
         return self.getParamString(title   = 'Start positions',
-                                   keys    = ['1','2','3'],
+                                   keys    = ['fast','med','slow'],
                                    mapAttr = self.start)
 
     def getGridsampString(self):
