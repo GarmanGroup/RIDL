1bd1fcb083b323efeb97c25833e8456ece25aab4
charliebury <csbury@me.com>
2017-02-25 18:57:41 +0000

    new code to check all required python packages to run RIDL now included

--- lib/checkDependencies.py
+++ lib/checkDependencies.py
@@ -7,9 +7,42 @@ class checkDependencies():
 	def __init__(self,
 				 checkAll = False):
 
+		pythonPackageList = ['sys',
+							 'argparse',
+							 'os',
+							 'time',
+							 'shutil',
+							 'numpy',
+							 'imp',
+							 'matplotlib',
+							 'math',
+							 'scipy',
+							 'numexpr',
+							 'string',
+							 'pandas',
+							 'operator',
+							 'warnings',
+							 'struct',
+							 'mmap',
+							 'random']
+
+		# pythonPackageList += ['sklearn']
+
+		allFound = True
 		if checkAll:
-			self.checkSeaborn(printText = True)
-			self.checkCCP4(printText = True)
+			found = self.checkSeaborn()
+			allFound *= found
+			found = self.checkCCP4()
+			allFound *= found
+			for pkg in pythonPackageList:
+				found = self.checkPythonPackage(packageName = pkg)
+				allFound *= found
+
+			if allFound == 1:
+				print 'All required dependencies have been successfully located'
+			else:
+				print 'Warning:\nOne or more required dependencies'+\
+					  ' not successfully located\nSee above for details.'
 
 	def checkCCP4(self,
 				  printText = False,
@@ -28,18 +61,47 @@ class checkDependencies():
 		os.system('aimless &> {}'.format(t))
 		l = open(t,'r').readlines()[0]
 		if l == 'sh: aimless: command not found\n':
-			if printText is True:
-
-				err = 'CCP4 program suite not found..\n'
-
-				self.printOrWriteToLog(logFile = logFile,
-							  	       txt     = err)
+			err = 'CCP4 program suite not found..\n'
+			self.printOrWriteToLog(logFile = logFile,
+						  	       txt     = err)
 			return False
 
 		if printText is True:	
 			ln = '---> success!\n'
 			self.printOrWriteToLog(logFile = logFile,
 							  	   txt     = ln)
+
+		return True
+
+	def checkPythonPackage(self,
+					       printText   = False,
+					       logFile     = '',
+					       packageName = 'seaborn'):
+
+		# check whether a python package is present and 
+		# flag if not. logFile is '' (do not write to log 
+		# file) or a logFile.py class object
+
+		if printText is True:
+
+			ln = 'Checking whether "{}" python package present...'.format(packageName)
+			self.printOrWriteToLog(logFile = logFile,
+							  	   txt     = ln)
+
+		try:
+			imp.find_module(packageName)
+		except ImportError:
+			err = 'Python package {} not found..\n'.format(packageName)+\
+			 	  'Recommendation:\n'+\
+			      'Try "pip install {}" to install?\n'.format(packageName)
+			self.printOrWriteToLog(logFile = logFile,
+						  	  	   txt     = err)
+			return False
+
+		if printText is True:	
+			ln = '---> success!\n'
+			self.printOrWriteToLog(logFile = logFile,
+							       txt     = ln)
 
 		return True
 
@@ -60,14 +122,11 @@ class checkDependencies():
 		try:
 			imp.find_module('seaborn')
 		except ImportError:
-			if printText is True:
-
-				err = 'Plotting library seaborn not found..\n'+\
-				 	  'Will not create any plots for current run.\n'+\
-				      'Use "pip install seaborn" to install for future use.\n'
-
-				self.printOrWriteToLog(logFile = logFile,
-							  	  	   txt     = err)
+			err = 'Plotting library seaborn not found..\n'+\
+			 	  'Will not create any plots for current run.\n'+\
+			      'Use "pip install seaborn" to install for future use.\n'
+			self.printOrWriteToLog(logFile = logFile,
+						  	  	   txt     = err)
 			return False
 
 		if printText is True:	
