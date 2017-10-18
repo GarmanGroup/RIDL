84067c86cf99df0a276dd128bfad9135e76810cb
Charlie Bury <csbury@me.com>
2017-10-18 16:26:08 +0100

    linted files

--- runRIDL_class.py
+++ runRIDL_class.py
@@ -1,119 +1,122 @@
-import sys,os,time
-sys.path.insert(0,'./lib')
+import sys
+import os
+import time
+sys.path.insert(0, './lib')
 from errors import error
 from processFiles import processFiles
 from logFile import logFile
-import shutil
+
 
 class process():
 
-	# a class to overview the generation of atom-tagged and
-	# density maps from pdb & mtz files for a specified damage
-	# series. This class contains some methods to write input 
-	# files, and can call the class processFiles, in which the 
-	# actual map-generation (and subsequent calculation of 
-	# per-atom damage metrics) can be performed
+    # a class to overview the generation of atom-tagged and
+    # density maps from pdb & mtz files for a specified damage
+    # series. This class contains some methods to write input
+    # files, and can call the class processFiles, in which the
+    # actual map-generation (and subsequent calculation of
+    # per-atom damage metrics) can be performed
 
-	def __init__(self,
-				 inputFile           = 'fullInput.txt',
-				 run                 = True,
-				 proceedToMetricCalc = False,
-				 skipToMetricCalc    = False,
-				 outputGraphs        = 'yes',
-				 cleanUpFinalFiles   = False,
-				 printOutput         = True,
-				 skipToSummaryFiles  = False,
-				 writeSummaryFiles   = False,
-				 keepMapDir          = True):
+    def __init__(self,
+                 inputFile='fullInput.txt',
+                 run=True,
+                 proceedToMetricCalc=False,
+                 skipToMetricCalc=False,
+                 outputGraphs='yes',
+                 cleanUpFinalFiles=False,
+                 printOutput=True,
+                 skipToSummaryFiles=False,
+                 writeSummaryFiles=False,
+                 keepMapDir=True):
 
-		self.inputFile           = inputFile
-		self.proceedToMetricCalc = proceedToMetricCalc
-		self.skipToMetricCalc    = skipToMetricCalc
-		self.outputGraphs        = outputGraphs # takes 'yes', 'no' or 'slim'
-		self.cleanUpFinalFiles   = cleanUpFinalFiles
-		self.printOutput         = printOutput
-		self.skipToSummaryFiles  = skipToSummaryFiles
-		self.writeSummaryFiles   = writeSummaryFiles
-		self.keepMapDir          = keepMapDir
+        self.inputFile = inputFile
+        self.proceedToMetricCalc = proceedToMetricCalc
+        self.skipToMetricCalc = skipToMetricCalc
+        # outputGraphs takes 'yes', 'no' or 'slim'
+        self.outputGraphs = outputGraphs
+        self.cleanUpFinalFiles = cleanUpFinalFiles
+        self.printOutput = printOutput
+        self.skipToSummaryFiles = skipToSummaryFiles
+        self.writeSummaryFiles = writeSummaryFiles
+        self.keepMapDir = keepMapDir
 
-		if run:
-			self.run()
+        if run:
+            self.run()
 
-	def setInputFile(self,name):
+    def setInputFile(self, name):
 
-		# specify input file name as required for the run
+        # specify input file name as required for the run
 
-		self.inputFile = name
-		print 'Input file name set as "{}"'.format(self.inputFile)
+        self.inputFile = name
+        print 'Input file name set as "{}"'.format(self.inputFile)
 
-	def checkInputFileExists(self):
+    def checkInputFileExists(self):
 
-		# check if input file exists and is readable 
+        # check if input file exists and is readable
 
-		if os.path.isfile(self.inputFile) and os.access(self.inputFile,os.R_OK):
-			# print "Input file exists and is readable"
-			return True
-		else:
-			err = "Either input file is missing or is not readable"
-			error(text = err)
-			return False
+        if (os.path.isfile(self.inputFile) and
+                os.access(self.inputFile, os.R_OK)):
+            return True
+        else:
+            error(text="Either input file is missing or is not readable")
+            return False
 
-	def run(self):
+    def run(self):
 
-		# run to process files with the specified input file
+        # run to process files with the specified input file
 
-		success = self.checkInputFileExists()
-		if not success: 
-			return success
+        success = self.checkInputFileExists()
+        if not success:
+            return success
 
-		self.startLogFile()
-		self.titleCaption()
-		self.info()
+        self.startLogFile()
+        self.titleCaption()
+        self.info()
 
-		pro = processFiles(inputFile           = self.inputFile,
-						   proceedToMetricCalc = self.proceedToMetricCalc,
-						   skipToMetricCalc    = self.skipToMetricCalc,
-						   outputGraphs        = self.outputGraphs,
-						   cleanFinalFiles     = self.cleanUpFinalFiles,
-						   logFileObj          = self.logFile,
-						   skipToSummaryFiles  = self.skipToSummaryFiles,
-						   writeSummaryFiles   = self.writeSummaryFiles,
-						   keepMapDir          = self.keepMapDir)
+        pro = processFiles(inputFile=self.inputFile,
+                           proceedToMetricCalc=self.proceedToMetricCalc,
+                           skipToMetricCalc=self.skipToMetricCalc,
+                           outputGraphs=self.outputGraphs,
+                           cleanFinalFiles=self.cleanUpFinalFiles,
+                           logFileObj=self.logFile,
+                           skipToSummaryFiles=self.skipToSummaryFiles,
+                           writeSummaryFiles=self.writeSummaryFiles,
+                           keepMapDir=self.keepMapDir)
 
-		return pro.jobSuccess
+        return pro.jobSuccess
 
-	def printInputFile(self):
+    def printInputFile(self):
 
-		# print the contents of the specified input file
+        # print the contents of the specified input file
 
-		fileIn = open(self.inputFile,'r')
-		for line in fileIn.readlines():
-			if len(line.strip()) != 0:
-				print line.strip()
-		fileIn.close()
+        fileIn = open(self.inputFile, 'r')
+        for line in fileIn.readlines():
+            if len(line.strip()) != 0:
+                print line.strip()
+        fileIn.close()
 
-	def writeTemplateInputFile(self,
-							   numHigherDoseDatasets = 1):
+    def writeTemplateInputFile(self,
+                               numHigherDoseDatasets=1):
 
-		# write a template input file to current directory to be completed
+        # write a template input file to current directory to be completed
 
-		f = open(self.inputFile,'w')
-		highDoseStrs = [','.join(['???']*numHigherDoseDatasets)]*4
-		string   = 	'dir ???\n'+\
-					'\nINITIALDATASET\nname1 ???\nmtz1 ???\nmtzlabels1 ???\npdb1 ???\nRfreeFlag1 ???\n'+\
-					'\nLATERDATASET\nname2 {}\nmtz2 {}\nmtzlabels2 {}\npdb2 {}\n'.format(*highDoseStrs)+\
-					'\nPHASEDATASET\nname3 ???\nmtz3 ???\nphaseLabel ???\nFcalcLabel ???'+\
-					'\nMAPINFO\nsfall_VDWR 1\ndensMapType DIFF\nFFTmapWeight False\ndeleteIntermediateFiles TRUE'
-		f.write(string)
-		f.close()
-		print 'Template input file "{}" written'.format(self.inputFile)
+        f = open(self.inputFile, 'w')
+        v = [','.join(['???']*numHigherDoseDatasets)]*2
+        string = 'dir ???\n\nINITIALDATASET\nname1 ???\nmtz1 ???' +\
+                 '\nmtzlabels1 ???\npdb1 ???\nRfreeFlag1 ???\n' +\
+                 '\nLATERDATASET\nname2 {}\nmtz2 {}\nmtzlabels2 '.format(*v) +\
+                 '{}\npdb2 {}\n'.format(*v) +\
+                 '\nPHASEDATASET\nname3 ???\nmtz3 ???\nphaseLabel ' +\
+                 '???\nFcalcLabel ???'
+        f.write(string)
+        f.close()
+        print 'Template input file "{}" written'.format(self.inputFile)
 
-	def howToWriteInputFile(self,
-							printStr = True):
+    def howToWriteInputFile(self,
+                            printStr=True):
 
-		# some information on how write the input file
+        # some information on how write the input file
 
-		infoString 	= """
+        infoString = """
 *** Information on how to successfully write an input file for job ***
 
 * Note:
@@ -122,128 +125,122 @@ class process():
 * section below:
 
 FILELOCATION
-	dir 		: full path to output working directory
+    dir         : full path to output working directory
 
 INITIALDATASET
-	name1 		: assign a name to your low dose damage set
-	mtz1  		: full path to the low dose mtz file
-	mtzlabels1	: F & SIGF column labels (look in low dose mtz file), if "FP_X" then type "FP_X"
-	pdb1 		: full path to the low dose pdb file
-	RfreeFlag1 	: the Rfree flag label within the low dose mtz file (e.g. "FreeR_flag")
+    name1       : assign a name to your low dose damage set
+    mtz1        : full path to the low dose mtz file
+    mtzlabels1  : F & SIGF column labels (look in low dose mtz file), if "FP_X" then type "FP_X"
+    pdb1        : full path to the low dose pdb file
+    RfreeFlag1  : the Rfree flag label within the low dose mtz file (e.g. "FreeR_flag")
 
 LATERDATASET
-	name2 		: assign a name to your high dose damage set (e.g. a pdb code "1qid")
-	mtz2 		: full path to the high dose mtz file
-	mtzlabels2 	: F & SIGF column labels (look in high dose mtz file), if "FP_X" then type "FP_X"
-	pdb2 		: full path to the high dose pdb file
+    name2       : assign a name to your high dose damage set (e.g. a pdb code "1qid")
+    mtz2        : full path to the high dose mtz file
+    mtzlabels2  : F & SIGF column labels (look in high dose mtz file), if "FP_X" then type "FP_X"
+    pdb2        : full path to the high dose pdb file
 
 PHASEDATASET
-	name3 		: assign a name to your low dose damage set (same as INITIALDATASET above)
-	mtz3 		: full path to the low dose mtz file (same as INITIALDATASET above)
-	phaseLabel 	: PHI phase column labels (look in low dose mtz file), if "PHIC_X" then type "PHIC_X"
-	FcalcLabel  : calculated structure amplitudes from same file as above, if "FC_X" then type "FC_X"
+    name3       : assign a name to your low dose damage set (same as INITIALDATASET above)
+    mtz3        : full path to the low dose mtz file (same as INITIALDATASET above)
+    phaseLabel  : PHI phase column labels (look in low dose mtz file), if "PHIC_X" then type "PHIC_X"
+    FcalcLabel  : calculated structure amplitudes from same file as above, if "FC_X" then type "FC_X"
 
 MAPINFO
-	For difference map analysis, do not change these parameters, contact the author for further
-	information before varying these
-	"""
-		if printStr:
-			print infoString
+    For difference map analysis, do not change these parameters, contact the author for further
+    information before varying these
+    """
+        if printStr:
+            print infoString
 
-		return infoString
+        return infoString
 
-	def fillerLine(self):
+    def fillerLine(self):
 
-		# a simple filler line to print to command line
+        # a simple filler line to print to command line
 
-		ln = '---------------------------------------------------------------'	
-		self.logFile.writeToLog(str = ln)
+        ln = '---------------------------------------------------------------'
+        self.logFile.writeToLog(str=ln)
 
+    def titleCaption(self,
+                     whichString='new'):
 
-	def titleCaption(self,
-					 whichString = 'new'):
+        # a title line to print to command line
 
-		# a title line to print to command line
+        if whichString == 'old':
+            txt = '\n||========================== RIDL ==========================||' +\
+                  '||======== Radiation-Induced Density Loss Analysis =========||\n'
+        else:
+            txt = '\n----------------------------------------------------\n' +\
+                  ' '*24+'RIDL\n' +\
+                  ' '*4+'(Radiation-Induced Density Loss Analysis)\n' +\
+                  '----------------------------------------------------'
 
-		if whichString == 'old':
-			txt = '\n||========================== RIDL ==========================||'+\
-			      '||======== Radiation-Induced Density Loss Analysis =========||\n'
-		else:
-			txt = '\n----------------------------------------------------\n'+\
-				  ' '*24+'RIDL\n'+\
-				  ' '*4+'(Radiation-Induced Density Loss Analysis)\n'+\
-				  '----------------------------------------------------'
+        self.logFile.writeToLog(str=txt)
 
-		self.logFile.writeToLog(str = txt)
+    def info(self):
 
-	def info(self):
+        # print a small ammount of information to the
+        # command line (date and email)
 
-		# print a small ammount of information to the 
-		# command line (date and email)
+        txt = 'date: {}\n'.format(time.strftime("%c")) +\
+              'email queries to charles.bury@dtc.ox.ac.uk\n'
+        self.logFile.writeToLog(str=txt)
 
-		txt = 'date: {}\n'.format(time.strftime("%c"))+\
-			  'email queries to charles.bury@dtc.ox.ac.uk\n'
-		self.logFile.writeToLog(str = txt)
+    def quickParseInputFile(self):
 
-	def quickParseInputFile(self):
+        # parse input file to retrieve directory name for output
 
-		# parse input file to retrieve directory name for output
+        f = open(self.inputFile, 'r')
+        for l in f.readlines():
+            if len(l) == 0:
+                continue
+            if 'dir' == l.split()[0]:
+                self.outputDir = l.split()[1]
+                if self.outputDir[-1] not in ('/'):
+                    self.outputDir += '/'
+                return
 
-		f = open(self.inputFile,'r')
-		for l in f.readlines():
-			if len(l) == 0:
-				continue
-			if 'dir' == l.split()[0]:
-				self.outputDir = l.split()[1]
-				if self.outputDir[-1] not in ('/'):
-					self.outputDir  += '/'
-				return
+    def startLogFile(self):
 
-	def startLogFile(self):
+        # create log file for the current job
 
-		# create log file for the current job
+        self.quickParseInputFile()
+        self.checkOutputDirExists(printToScreen=self.printOutput)
 
-		self.quickParseInputFile()
-		self.checkOutputDirExists(printToScreen = self.printOutput)
+        if self.outputDir[-1] == '/':
+            logDir = self.outputDir[:-1]+'/RIDL-log/'
+        else:
+            logDir = self.outputDir+'/RIDL-log/'
 
-		if self.outputDir[-1] == '/':
-			logDir = self.outputDir[:-1]+'/RIDL-log/'
-		else:
-			logDir = self.outputDir+'/RIDL-log/'
+        if not os.path.exists(logDir):
+            os.makedirs(logDir)
 
-		if not os.path.exists(logDir):
-			os.makedirs(logDir)
+        name = '{}RIDLjob.log'.format(logDir)
 
-		name = '{}RIDLjob.log'.format(logDir)
+        logName = lambda x: name.replace('.log', '_{}{}'.format(x, '.log'))
+        i = 1
+        while os.path.isfile(logName(i)):
+            i += 1
+        uniqLogName = logName(i)
 
-		logName = lambda x: name.replace('.log','_{}{}'.format(x,'.log'))
-		i = 1
-		while os.path.isfile(logName(i)): 
-			i += 1 
-		uniqLogName = logName(i)
+        log = logFile(fileName=uniqLogName, fileDir=self.outputDir,
+                      printToScreen=self.printOutput)
+        self.logFile = log
 
-		log = logFile(fileName      = uniqLogName,
-					  fileDir       = self.outputDir,
-					  printToScreen = self.printOutput)
-		self.logFile = log
+    def checkOutputDirExists(self,
+                             printToScreen=True):
 
-	def checkOutputDirExists(self,
-							 printToScreen = True):
+        # check whether output directory exists and make if not
 
-		# check whether output directory exists and make if not
+        if not self.outputDir.endswith('/'):
+            if printToScreen:
+                print 'Working directory specified in input ' +\
+                      'file must end in "/" - appending.'
+            self.outputDir += '/'
 
-		if not self.outputDir.endswith('/'):
-			if printToScreen:
-				print 'Working directory specified in input '+\
-					  'file must end in "/" - appending.'
-			self.outputDir += '/'
-
-		if not os.path.exists(self.outputDir):
-			if printToScreen:
-				print 'Output directory "{}" not found, making directory'.format(self.outputDir)
-			os.makedirs(self.outputDir)
-
-
-
-
-
+        if not os.path.exists(self.outputDir):
+            if printToScreen:
+                print 'Output directory "{}" '.format(self.outputDir) +\
+                      'not found, making directory'
+            os.makedirs(self.outputDir)
--- runRIDL_metricCalc.py
+++ runRIDL_metricCalc.py
@@ -1,131 +1,121 @@
 import sys
-sys.path.insert(0,'./lib')
+sys.path.insert(0, './lib')
 from calculateMetrics import calculateMetrics
 import os
 
+
 class run():
 
-	# run the metric calculation step of the RIDL pipeline
-	#  to calculate per-atom density metrics for a series 
-	# of increasing doses
+    # run the metric calculation step of the RIDL pipeline
+    #  to calculate per-atom density metrics for a series
+    # of increasing doses
 
-	def __init__(self,
-				 calculate      = True,
-				 inputFileLoc   = '',
-				 logFile        = '',
-				 skipToSumFiles = False,
-				 writeSumFiles  = False,
-				 inclFCmets     = False):
+    def __init__(self,
+                 calculate=True, inputFileLoc='',
+                 logFile='', skipToSumFiles=False,
+                 writeSumFiles=False, inclFCmets=False):
 
-		self.inputFileName = inputFileLoc + 'metricCalc_inputfile.txt'
-		self.logFile       = logFile
-		
-		if calculate:
-			self.runCalculateMetrics(sumFiles   = writeSumFiles,
-									 inclFCmets = inclFCmets)
+        self.inputFileName = inputFileLoc + 'metricCalc_inputfile.txt'
+        self.logFile = logFile
 
-		if skipToSumFiles:
-			self.runCalculateMetrics(mapProcess  = False,
-									 postProcess = False,
-									 retrieve    = True,
-									 sumFiles    = True,
-									 inclFCmets  = inclFCmets)
+        if calculate:
+            self.runCalculateMetrics(sumFiles=writeSumFiles,
+                                     inclFCmets=inclFCmets)
 
-	def runCalculateMetrics(self,
-				            mapProcess  = True,
-				  			postProcess = True,
-				  			retrieve    = False,
-				  			sumFiles    = False,
-				  			inclFCmets  = False):
+        if skipToSumFiles:
+            self.runCalculateMetrics(mapProcess=False, postProcess=False,
+                                     retrieve=True, sumFiles=True,
+                                     inclFCmets=inclFCmets)
 
-		# run the metric calculation scripts for the currently 
-		# defined input file
-		ln = '\n\n**** METRIC CALCULATIONS ****\n'
-		self.logFile.writeToLog(str = ln)
+    def runCalculateMetrics(self,
+                            mapProcess=True, postProcess=True,
+                            retrieve=False, sumFiles=False,
+                            inclFCmets=False):
 
-		exists = self.checkInputFileExists()
-		if not exists:
-			return
+        # run the metric calculation scripts for
+        # the currently defined input file
+        self.logFile.writeToLog(str='\n\n**** METRIC CALCULATIONS ****\n')
 
-		eT = calculateMetrics(logFile    = self.logFile,
-							  sumFiles   = sumFiles,
-							  inclFCmets = inclFCmets)
+        exists = self.checkInputFileExists()
+        if not exists:
+            return
 
-		eT.runPipeline(map_process   = mapProcess,
-					   post_process  = postProcess,
-					   retrieve      = retrieve,
-					   inputFileName = self.inputFileName)
-		self.et = eT
+        eT = calculateMetrics(logFile=self.logFile, sumFiles=sumFiles,
+                              inclFCmets=inclFCmets)
 
-	def defineDoseList(self,doses,names,version):
+        eT.runPipeline(map_process=mapProcess, post_process=postProcess,
+                       retrieve=retrieve, inputFileName=self.inputFileName)
+        self.et = eT
 
-		# do not include first dataset dose if difference maps chosen
+    def defineDoseList(self, doses, names, version):
 
-		if version == 'DIFF':
-			if len(doses.split(',')[1:])>1:
-				dosesOut = ','.join(doses.split(',')[1:])
-				namesOut = ','.join(names.split(',')[1:])
-			else:
-				dosesOut = doses.split(',')[1]
-				namesOut = names.split(',')[1]
-			return dosesOut,namesOut
-		else: return doses,names
+        # do not include first dataset dose if difference maps chosen
 
-	def writeBlankInputFile(self):
+        if version == 'DIFF':
+            if len(doses.split(',')[1:]) > 1:
+                dosesOut = ','.join(doses.split(',')[1:])
+                namesOut = ','.join(names.split(',')[1:])
+            else:
+                dosesOut = doses.split(',')[1]
+                namesOut = names.split(',')[1]
+            return dosesOut, namesOut
+        else:
+            return doses, names
 
-		# Need to create input file for a generic damage 
-		# series to be completed by the user
+    def writeBlankInputFile(self):
 
-		args = ['inDir <input file location>',
-				'outDir <output file location>',
-				'damageset_name <consistent name of series, e.g. for TRAP1.pdb, TRAP2.pdb, this is TRAP>',
-				'damageset_num <dataset numbers, e.g. 1,2 for TRAP. Can be letters corresponding to pdb series>',
-				'initialPDB <initial dataset pdb file e.g. TRAP1.pdb>',
-				'doses <list of doses, length must match length of damageset_num above>',
-				'PKLMULTIFILE <if already processed in RIDL, can specify single output .pkl for series>']
-		inputString = self.writeInputFile(*args)	
+        # Need to create input file for a generic damage
+        # series to be completed by the user
 
-	def writeInputFile(self,
-					   inDir         = '',
-					   outDir        = '',
-					   damSetName    = '',
-					   laterDatasets = '',
-					   initialPDB    = '',
-					   doses         = '',
-					   PKLMULTIFILE  = '',
-					   outputGraphs  = 'yes'):
+        args = ['inDir <input file location>',
+                'outDir <output file location>',
+                'damageset_name <consistent name of series, ' +
+                'e.g. for TRAP1.pdb, TRAP2.pdb, this is TRAP>',
+                'damageset_num <dataset numbers, e.g. 1,2 for ' +
+                'TRAP. Can be letters corresponding to pdb series>',
+                'initialPDB <initial dataset pdb file e.g. TRAP1.pdb>',
+                'doses <list of doses, length must match length ' +
+                'of damageset_num above>',
+                'PKLMULTIFILE <if already processed in RIDL, can ' +
+                'specify single output .pkl for series>']
+        self.writeInputFile(*args)
 
-		# write a generic input file for a damage series here
+    def writeInputFile(self,
+                       inDir='', outDir='', damSetName='',
+                       laterDatasets='', initialPDB='', doses='',
+                       PKLMULTIFILE='', outputGraphs='yes'):
 
-		inputString = 'inDir {}\n'.format(inDir)+\
-					  'outDir {}\n'.format(outDir)+\
-					  'damageset_name {}\n'.format(damSetName)+\
-					  'laterDatasets {}\n'.format(laterDatasets)+\
-					  'initialPDB {}\n'.format(initialPDB)+\
-					  'doses {}\n'.format(doses)
+        # write a generic input file for a damage series here
 
-		if PKLMULTIFILE != '':
-			inputString += 'PKLMULTIFILE {}'.format(PKLMULTIFILE)
+        inputString = 'inDir {}\n'.format(inDir) +\
+                      'outDir {}\n'.format(outDir) +\
+                      'damageset_name {}\n'.format(damSetName) +\
+                      'laterDatasets {}\n'.format(laterDatasets) +\
+                      'initialPDB {}\n'.format(initialPDB) +\
+                      'doses {}\n'.format(doses)
 
-		if outputGraphs == 'yes':
-			inputString += 'plot'
-		elif outputGraphs == 'slim':
-			inputString += 'slim-plot'
+        if PKLMULTIFILE != '':
+            inputString += 'PKLMULTIFILE {}'.format(PKLMULTIFILE)
 
-		inputFile  = open(self.inputFileName,'w')
-		inputFile.write(inputString)
-		inputFile.close()	
+        if outputGraphs == 'yes':
+            inputString += 'plot'
+        elif outputGraphs == 'slim':
+            inputString += 'slim-plot'
 
-	def checkInputFileExists(self):
+        inputFile = open(self.inputFileName, 'w')
+        inputFile.write(inputString)
+        inputFile.close()
 
-		# check whether input file present for the metric 
-		# calculation step of the RIDL pipeline
+    def checkInputFileExists(self):
 
-		if os.path.isfile(self.inputFileName) and os.access(self.inputFileName,os.R_OK):
-			return True
-		else:
-			print "Either input file is missing or is not readable"
-			print 'Check whether "{}" is a suitable input file'.format(self.inputFileName)
-			return False
+        # check whether input file present for the metric
+        # calculation step of the RIDL pipeline
 
-
+        if (os.path.isfile(self.inputFileName) and
+                os.access(self.inputFileName, os.R_OK)):
+            return True
+        else:
+            print "Either input file is missing or is not readable"
+            print 'Check whether "{}" is a '.format(self.inputFileName) +\
+                  'suitable input file.'
+            return False
